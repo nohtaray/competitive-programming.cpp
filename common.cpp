b192d383de3b016d6511675739900aa92f651331
yada <ydt.hran2@gmail.com>
2019-08-16 20:48:08 +0900

    Initial commit

---
+++ common.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep2(i, s, e) for (int i = (s); i < (int)(e); ++i)
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+int main() {
+  ifstream ifs;
+  if (getenv("LOCAL") != nullptr) {
+    ifs.open("src/_in.txt");
+    cin.rdbuf(ifs.rdbuf());
+  }
+
+}
