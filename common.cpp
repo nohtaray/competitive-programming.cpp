707810dfc2833afed87cbbaf563c413d618e6a66
Yada <ydt.hran2@gmail.com>
2019-08-16 22:00:59 +0900

    Update common.cpp

--- common.cpp
+++ common.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define rep2(i, s, e) for (int i = (s); i < (int)(e); ++i)
+#define rep(i, n) for (int (i) = 0; (i) < (int)(n); ++(i))
+#define rep2(i, s, e) for (int (i) = (s); (i) < (int)(e); ++(i))
 using namespace std;
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
+const long long MOD = 1000000007;
 
 int main() {
   ifstream ifs;
