e56faff4b6275e5585f897b0b681109874eabfa1
Yada <ydt.hran2@gmail.com>
2019-08-17 14:07:28 +0900

    Update common.cpp

--- common.cpp
+++ common.cpp
@@ -1,13 +1,18 @@
 #include <bits/stdc++.h>
+#include <boost/multiprecision/cpp_int.hpp>
 #define rep(i, n) for (int (i) = 0; (i) < (int)(n); ++(i))
 #define rep2(i, s, e) for (int (i) = (s); (i) < (int)(e); ++(i))
 using namespace std;
+using namespace boost::multiprecision;
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
+using lll = cpp_int;
 const long long MOD = 1000000007;
 
 int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
   ifstream ifs;
   if (getenv("LOCAL") != nullptr) {
     ifs.open("src/_in.txt");
