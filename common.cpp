506bb666b94d8ff61d1e1e13555f1bbde1269c25
Yada <ydt.hran2@gmail.com>
2019-08-20 14:01:16 +0900

    Update common.cpp

--- common.cpp
+++ common.cpp
@@ -1,14 +1,20 @@
 #include <bits/stdc++.h>
 #include <boost/multiprecision/cpp_int.hpp>
-#define rep(i, n) for (int (i) = 0; (i) < (int)(n); ++(i))
-#define rep2(i, s, e) for (int (i) = (s); (i) < (int)(e); ++(i))
+#define rep(i, n) repr((i), 0, (n))
+#define repr(i, a, t) reps((i), (a), (t), 1)
+#define reps(i, a, t, s) for (long long (i) = (a); (i) < (long long)(t); (i) += (s))
+#define rrep(i, n) rrepr((i), (n) - 1, 0)
+#define rrepr(i, a, t) rreps((i), (a), (t), 1)
+#define rreps(i, a, t, s) for (long long (i) = (a); (i) >= (long long)(t); (i) -= (s))
+#define each(v, c) for (auto &&(v) : (c))
 using namespace std;
 using namespace boost::multiprecision;
 using ll = long long;
 using ull = unsigned long long;
 using ld = long double;
-using lll = cpp_int;
-const long long MOD = 1000000007;
+using ml = cpp_int;
+const long long MOD = 1e9 + 7;
+const long long INF = 1e18;
 
 int main() {
   cin.tie(nullptr);
