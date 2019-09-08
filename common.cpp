6576867e05f4de3d2d873bc7455890be0b823fd9
Yada <ydt.hran2@gmail.com>
2019-09-08 23:21:20 +0900

    Make the template compact

--- common.cpp
+++ common.cpp
@@ -2,6 +2,15 @@
 #include <boost/format.hpp>
 #include <boost/integer/common_factor_rt.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
+#include <boost/optional.hpp>
+
+#ifdef DEBUG
+#include "libs/debug.cpp"
+#define DEBUG 1
+#else
+#define DEBUG 0
+#endif
+
 #define rep(i, n) repr((i), 0, (n))
 #define repr(i, a, t) reps((i), (a), (t), 1)
 #define reps(i, a, t, s) for (long long (i) = (a); (i) < (long long)(t); (i) += (s))
@@ -10,6 +19,7 @@
 #define rreps(i, a, t, s) for (long long (i) = (a); (i) >= (long long)(t); (i) -= (s))
 #define each(v, c) for (auto &&(v) : (c))
 #define all(c) (c).begin(), (c).end()
+
 using namespace std;
 namespace mp = boost::multiprecision;
 using ll = long long;
@@ -22,41 +32,12 @@ const auto none = boost::none;
 const long long MOD = 1e9 + 7;
 const long long INF = 1e18;
 
-template<typename T>
-ostream &operator<<(ostream &os, const vector<T> &v) {
-  if (v.empty()) return os << "vector{}";
-  os << "vector{" << *v.begin();
-  for (auto it = ++v.begin(); it != v.end(); ++it)
-    os << ", " << *it;
-  return os << "}";
-}
-template<typename T>
-ostream &operator<<(ostream &os, const set<T> &s) {
-  if (s.empty()) return os << "set{}";
-  os << "set{" << *s.begin();
-  for (auto it = ++s.begin(); it != s.end(); ++it)
-    os << ", " << *it;
-  return os << "}";
-}
-template<typename S, typename T>
-ostream &operator<<(ostream &os, const map<S, T> &m) {
-  if (m.empty()) return os << "map{}";
-  os << "map{" << (*m.begin()).first << ": " << (*m.begin()).second;
-  for (auto it = ++m.begin(); it != m.end(); ++it)
-    os << ", " << (*it).first << ": " << (*it).second;
-  return os << "}";
-}
-template<typename S, typename T>
-ostream &operator<<(ostream &os, const pair<S, T> &p) {
-  return os << "pair{" << p.first << ", " << p.second << "}";
-}
-
 int main() {
   cout << fixed << setprecision(10);
   cin.tie(nullptr);
   ios::sync_with_stdio(false);
   ifstream ifs;
-  if (getenv("LOCAL") != nullptr) {
+  if (DEBUG) {
     ifs.open("src/_in.txt");
     cin.rdbuf(ifs.rdbuf());
   }
--- debug.cpp
+++ debug.cpp
@@ -1,6 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<typename T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+  if (v.empty()) return os << "vector{}";
+  os << "vector{" << *v.begin();
+  for (auto it = ++v.begin(); it != v.end(); ++it)
+    os << ", " << *it;
+  return os << "}";
+}
+template<typename T>
+ostream &operator<<(ostream &os, const set<T> &s) {
+  if (s.empty()) return os << "set{}";
+  os << "set{" << *s.begin();
+  for (auto it = ++s.begin(); it != s.end(); ++it)
+    os << ", " << *it;
+  return os << "}";
+}
+template<typename S, typename T>
+ostream &operator<<(ostream &os, const map<S, T> &m) {
+  if (m.empty()) return os << "map{}";
+  os << "map{" << (*m.begin()).first << ": " << (*m.begin()).second;
+  for (auto it = ++m.begin(); it != m.end(); ++it)
+    os << ", " << (*it).first << ": " << (*it).second;
+  return os << "}";
+}
+template<typename T>
+ostream &operator<<(ostream &os, const unordered_set<T> &s) {
+  if (s.empty()) return os << "set{}";
+  os << "set{" << *s.begin();
+  for (auto it = ++s.begin(); it != s.end(); ++it)
+    os << ", " << *it;
+  return os << "}";
+}
+template<typename S, typename T>
+ostream &operator<<(ostream &os, const unordered_map<S, T> &m) {
+  if (m.empty()) return os << "map{}";
+  os << "map{" << (*m.begin()).first << ": " << (*m.begin()).second;
+  for (auto it = ++m.begin(); it != m.end(); ++it)
+    os << ", " << (*it).first << ": " << (*it).second;
+  return os << "}";
+}
+template<typename S, typename T>
+ostream &operator<<(ostream &os, const pair<S, T> &p) {
+  return os << "pair{" << p.first << ", " << p.second << "}";
+}
+
 /**
  * 2 進数表記
  * @tparam T
