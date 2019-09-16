cfcae9f27bd7ebfa5b14801f51a65b8d1a1f4403
Yada <ydt.hran2@gmail.com>
2019-09-16 17:56:46 +0900

    Add dijkstra

--- graph.cpp
+++ graph.cpp
@@ -1,5 +1,42 @@
 #include <bits/stdc++.h>
+#include <boost/optional.hpp>
+
 using namespace std;
+
+boost::optional<long long> dijkstra(
+    const vector<vector<tuple<long long, long long>>> &graph,
+    long long from_i,
+    long long to_i) {
+  const long long INF = 1e18;
+
+  vector<long long> dist(graph.size(), INF);
+  dist[from_i] = 0;
+
+  priority_queue<tuple<long long, long long>, vector<tuple<long long, long long>>, greater<>> que;
+  que.push(make_tuple(0, from_i));
+  while (!que.empty()) {
+    long long d, v;
+    tie(d, v) = que.top();
+    que.pop();
+    if (d > dist[v]) continue;
+    if (v == to_i) return d;
+
+    for (const auto &edge : graph[v]) {
+      long long w, u;
+      tie(u, w) = edge;
+      if (d + w < dist[u]) {
+        dist[u] = d + w;
+        que.push(make_tuple(d + w, u));
+      }
+    }
+  }
+
+  if (dist[to_i] < INF) {
+    return dist[to_i];
+  } else {
+    return boost::none;
+  }
+}
 
 vector<long long> topological_sort(const vector<vector<long long>> &graph) {
   long long size = graph.size();
